167f2e9da0540580091cc19489df01cab085dfc9
Phan Van Tai <vantai2004kt@gmail.com>
2025-11-13 23:28:39 +0700

    feat: update vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ví dụ: /my-vite-app/
+  base: "/React-Authentication-with-JWT-Access-Refresh-/", // ví dụ: /my-vite-app/
 })
