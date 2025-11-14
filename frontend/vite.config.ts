cdf7d1b0a3e88097de748891714f0d9dcd43ef2b
Phan Van Tai <vantai2004kt@gmail.com>
2025-11-14 10:06:50 +0700

    fix: re-write vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Authentication-with-JWT-Access-Refresh-/", // ví dụ: /my-vite-app/
 })
